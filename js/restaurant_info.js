0d9e90e5fffb5e0bf598f614201c28bcce40cbd5
DuyDo <duydo.dev@gmail.com>
2018-09-27 22:33:24 -0500

    add token

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZHV5ZG9kZXYiLCJhIjoiY2ptbGc3NzltMDgwajNxbnlka3I3OGoyNCJ9.CoPsSK7T-XQxVi1Q_7vW1w',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
